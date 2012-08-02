19bc522880e8f8f892a4b5e519b874552580b91e
apopple <alistair.popple@gmail.com>
2012-08-02 23:24:04 +1000

    Updated the application to support booting two processors simultaneously with the same memory mapping.
    Note that there is a possible race which means this could fail. It will also fail if LED D2 is turned on before any of the processors boot as it uses LED D2 as a very poorly implemented mutex. A better solution would be needed for real (non-demo) code.

--- Application/main.c
+++ Application/main.c
@@ -2,26 +2,49 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+volatile int *GPIO1_DATAOUT;
+volatile int *GPIO1_OE;
+
 static void LedFlash(void *Parameters)
 {
-  int i;
   portTickType LastWake;
 
-  *(int *) 0xfff10134 &= ~(1 << 8);
+  *GPIO1_OE &= ~(1 << 8);
 
   LastWake = xTaskGetTickCount();
 
   while(1)
   {
-    *(int *) 0xfff1013c ^= (1 << 8);
+    *GPIO1_DATAOUT ^= (1 << 8);
     vTaskDelayUntil(&LastWake, 1000);
   }
 }
 
 int main()
 {
-  xTaskCreate(LedFlash, "LedFlash", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+  volatile int i;
+  GPIO1_DATAOUT = (int *) 0xfff1013c;
+  GPIO1_OE = (int *) 0xfff10134;
+
+  *GPIO1_OE &= ~(1 << 8);
+
+  /* Only continue execution on one core ... there is an obvious race condition here howver so we need a better
+     way of determining which processor we are running on. But this quick and nasty hack works for demo
+     purposes. */
+  if( *GPIO1_DATAOUT & (1 << 8))
+  {
+    while(1);
+  }
+
+  *GPIO1_DATAOUT |= (1 << 8);
+
+  xTaskCreate(LedFlash, (signed char *) "LedFlash", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
   vTaskStartScheduler();
 
-  while( 1 );
+  /* Just sit and flash the LED quickly if we fail */
+  while( 1 )
+  {    
+    for( i = 0; i < 100000; i++ );
+    *GPIO1_DATAOUT ^= (1 << 8);
+  }
 }
